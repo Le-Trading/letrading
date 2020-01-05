a9dd2be81654f4c062fb44a7c6a4a7d272ed0d58
anthonypiquard <piquardanthony@gmail.com>
2020-01-05 15:25:10 +0100

    ajout template form twig + modifications infos user

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -3,11 +3,16 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\AccountType;
+use App\Entity\PasswordUpdate;
 use App\Form\RegistrationType;
+use App\Form\PasswordUpdateType;
+use Symfony\Component\Form\FormError;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
@@ -69,6 +74,93 @@ class AccountController extends AbstractController
 
         return $this->render('account/registration.html.twig', [
             'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * Modification de profil
+     *
+     * @Route("/account/profile", name="account_profile")
+     * @IsGranted("ROLE_USER")
+     * 
+     * @return Response
+     */
+    public function profile(Request $request, EntityManagerInterface $manager){
+        $user = $this->getUser();
+
+        $form = $this->createForm(AccountType::class, $user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $manager->persist($user);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "Votre compte a bien été mis à jour !"
+            );
+        }
+
+        return $this->render('account/profile.html.twig',[
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * modifier le mdp
+     *
+     * @Route("/account/password-update", name="account_password")
+     * @IsGranted("ROLE_USER")
+     * 
+     * @return Response
+     */
+    public function updatePassword(Request $request, EntityManagerInterface $manager, UserPasswordEncoderInterface $encoder){
+        $passwordUpdate = new PasswordUpdate();
+
+        $user = $this->getUser();
+
+        $form = $this->createForm(PasswordUpdateType::class, $passwordUpdate);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            if(!password_verify($passwordUpdate->getOldPassword(), $user->getHash())){
+                $form->get('oldPassword')->addError(new FormError("Votre mot de passe n'est pas valide !"));
+            }else{
+                $newPassword = $passwordUpdate->getNewPassword();
+                $hash = $encoder->encodePassword($user, $newPassword);
+
+                $user->setHash($hash);
+
+                $manager->persist($user);
+                $manager->flush();
+    
+                $this->addFlash(
+                    'success',
+                    "Votre mot de passe a bien été mis à jour !"
+                );
+
+                return $this->redirectToRoute('homepage');
+            }
+        }
+
+        return $this->render('account/password.html.twig',[
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * Permet d'afficher le profil de l'user connecté
+     *
+     * @Route("/account", name="account_index")
+     * @IsGranted("ROLE_USER")
+     * 
+     * @return Response
+     */
+    public function myAccount(){
+        return $this->render('account/index.html.twig',[
+            'user' => $this->getUser()
         ]);
     }
 }
